62a046e26a4a4d685e9ae89a2dd5f1ad3d340657
Achyut Bhagwat <161940049+AchyutBhagwat@users.noreply.github.com>
2024-07-02 08:44:37 +0000

    Datatypes

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,25 @@
+"use strict"; //  Treat all js code as newer version
+// alert(3+3)   //  We are using nodejs, not browser so it doesn't work
+
+// console.log(3
+//     +            //code readiability should be high
+//     3)
+
+console.log("Achyut");
+
+let name="Achyut"
+let age =21
+let isLoggedIn= false
+
+//number =>2 to power 53
+//bigint
+//String=> " "
+//boolean=> true/false
+//null=>Standalone value
+// undefined=> 
+// symbol =>unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //Object
