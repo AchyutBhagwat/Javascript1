567e8d84da983d75f5b7759f2db5ee43c582115a
Achyut Bhagwat <161940049+AchyutBhagwat@users.noreply.github.com>
2024-07-03 07:16:07 +0000

    Datatype Summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,32 @@
+// [premitive]
+//7- String, Number, Boolean, null, undefined, symbol, BigInt
+
+const score=100
+const scorevalue=100.3
+
+const isLoggedIn= false
+const outSidetemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+const bigNumber=2324325345456456212n
+
+// reference (Non Premitive)
+// Array, Object, Function
+
+const heros=["Shaktiman", "naagraj", "doga"]
+
+let myObj={
+    name:"Achyut",
+    age:21,
+}
+
+const myFunction=function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outSidetemp);
+console.log(typeof myFunction);
