71fab869738e5becbb16fc8ea72ddf1a95658afb
Achyut Bhagwat <161940049+AchyutBhagwat@users.noreply.github.com>
2024-07-03 06:38:33 +0000

    Comparisions

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,21 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2!=1);
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null > 0);  //somtime ti convert to NaN and somtime convert to 0
+console.log(null == 0); //it will confuse us so avoid to write this type of code
+console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//===
+
+console.log("2" === 2);
