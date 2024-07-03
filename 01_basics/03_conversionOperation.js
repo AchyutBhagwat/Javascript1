0231e8c609928adb756299c3c429e05af8b21e8f
Achyut Bhagwat <161940049+AchyutBhagwat@users.noreply.github.com>
2024-07-03 05:58:01 +0000

    type_conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -2,19 +2,26 @@
 // let score=null
 // let score=undefined
 // let score=true
-let score="Achyut"
 
+/*
+let score="Achyut"
 console.log(typeof score);
 console.log(typeof(score));
+*/
 
+/*
 let valueInNumber=Number(score);
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+*/
+
 
 //"33"=> 33
 //"33abc"=> NaN
 //true=>1; false=>0
 
+
+/*
 // let isLoggedIn=1;
 // let isLoggedIn=0;
 // let isLoggedIn="";
@@ -25,8 +32,46 @@ console.log(booleanIsLoggedIn);
 //1=> true; 0=>false;
 // ""=>false
 // "Achyut"=>true;
+*/
 
+
+/*
 let someNumber=33;
 let stringNumber=String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+*/
+
+
+//*********************Operations ***************************//
+
+let value=3;
+let negValue= -value
+//console.log(negValue);
+
+// 
+
+let str1="Hello";
+let str2=" Achyut";
+let str3=str1+str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2+3);
+console.log(1+2+"3"); 
+console.log((3 + 4) * 5 % 3); // bad practice
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3;
+num1=num2=num3=2+2
+
+let gameCounter=100;
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+//Link to study
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
