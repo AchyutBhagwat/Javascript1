6f25df33f14be31e143aa4fd706435bd951f5220
Achyut Bhagwat <161940049+AchyutBhagwat@users.noreply.github.com>
2024-07-04 06:09:12 +0000

    Stack And heap

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -1,4 +1,4 @@
-// [premitive]
+/* // [premitive]
 //7- String, Number, Boolean, null, undefined, symbol, BigInt
 
 const score=100
@@ -30,3 +30,27 @@ const myFunction=function(){
 console.log(typeof bigNumber);
 console.log(typeof outSidetemp);
 console.log(typeof myFunction);
+
+*/
+
+// **********************************************
+
+//stack(primitive), heap(non primitive)
+
+let myName="Achyut";
+
+let lastName=myName;
+lastName="Bhagwat";
+
+console.log(myName);
+console.log(lastName);
+
+let userOne={
+    email:"achyut@gmail.com",
+    upi:"abc@ybl"
+}
+userTwo=userOne;
+userTwo.email="bhagwat@gmail.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
